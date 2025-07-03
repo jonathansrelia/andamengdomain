eaa1876fd74d3a43598a784b61607bd089711fc7
jonathansrelia <jonathan.s@reliasourcing.com>
2025-07-04 02:57:56 +0800

    first commit

---
+++ src/pages/Coins.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Coins = () => {
+  return (
+    <section>
+        <div>
+          here
+        </div>
+        <div>
+          there
+        </div>
+        
+      </section>
+  )
+}
+
+export default Coins

a298d4f2bb98b9714bd625bce7336f4366799a03
jonathansrelia <jonathan.s@reliasourcing.com>
2025-07-05 02:16:59 +0800

    update with sample view

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Coins from './pages/Coins'
-import Bonus from './pages/Bonus'
-import Rolls from './pages/Rolls'
+import { Routes, Route } from "react-router-dom";
+import Coins from "./pages/Coins";
+import Bonus from "./pages/Bonus";
+import Rolls from "./pages/Rolls";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/1" element={<Coins/>} />
-        <Route path="/2" element={<Bonus/>} />
-        <Route path="/3" element={<Rolls/>} />
+        <Route path="/1" element={<Coins />} />
+        <Route path="/2" element={<Bonus />} />
+        <Route path="/3" element={<Rolls />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,15 +1,34 @@
+import coin_bg from "../assets/bg.jpg";
+
 const Coins = () => {
   return (
-    <section>
-        <div>
-          here
-        </div>
-        <div>
-          there
-        </div>
-        
-      </section>
-  )
-}
+    <>
+      <div className="relative flex items-center w-screen h-screen justify-center">
+        {/* BG */}
+        <img
+          src={coin_bg}
+          alt=""
+          onMouseDown={(e) => {
+            e.preventDefault();
+          }}
+          className="w-full h-full bg-cover absolute top-0 left-0 z-[1]"
+        />
+        {/* END BG */}
+        <section className="z-[2]">
+          <div className="px-[10px] py-[15px] justify-center text-center border-2  bg-white rounded-[90px]">
+            500 Rolls + 300 coins
+          </div>
+          <br />
+          <button className="px-[10px] py-[15px] bg-amber-300 rounded-4xl border-2 border-black/40 text-6xl text-black">
+            $149.99
+            <button className="text-2xl text-red-600 relative bottom-5 line-through rotate-[45deg]">
+              $199.99
+            </button>
+          </button>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default Coins
+export default Coins;
